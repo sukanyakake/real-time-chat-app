6b6b877c7ef52bde8e2b32ad71ee88f9fc8875c3
sukanyakake <kdmrsukanya@gmail.com>
2025-12-21 17:40:24 +0530

    frontend-codebase added

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+
+const ChatPage = () => {
+
+
+  return (
+    <div>
+      Chatpage
+    </div>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+export const useAuthStore=create((set,get)=>({
+    authUser:{name:"John",_id:123,age:25},
+    isLoading:false,
+    isLoggedIn:false,
+    login:()=>{
+        console.log("we just logged in")
+        set({isLoggedIn:true,isLoading:true})
+    }
+}))
