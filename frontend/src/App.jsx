3f70a77a75106e47e221ffd37c8605d2632433a0
sukanyakake <kdmrsukanya@gmail.com>
2025-12-22 14:41:34 +0530

    signup frontend implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,34 @@
-import React, { useState } from 'react'
-import { Routes,Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Routes,Route, Navigate } from 'react-router-dom'
 import ChatPage from './pages/ChatPage'
 import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
 import { useAuthStore } from './store/useAuthStore.js'
+import PageLoader from './components/PageLoader.jsx'
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
-  const {isLoggedIn,login}=useAuthStore()
-  console.log(isLoggedIn)
+  const {checkAuth,isCheckingAuth,authUser}=useAuthStore()
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+  console.log(authUser)
+  if(isCheckingAuth){
+    return <PageLoader/>
+  }
   return (
     <div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
             {/* DECORATORS - GRID BG & GLOW SHAPES */}
   <div className="absolute inset-0 bg-[linear-gradient(...)] bg-[size:14px_24px] pointer-events-none" />
 <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px] pointer-events-none" />
 <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px] pointer-events-none" />
+<Toaster/>
 
-
-    <button className='btn btn-primary' onClick={login}>{isLoggedIn?"Login in":"Logout"}</button>
 
     <Routes>
-      <Route path="/" element={<ChatPage />}></Route>
-      <Route path="/login" element={<LoginPage/>}></Route>
-      <Route path="/signup" element={<SignUpPage/>}></Route>
+      <Route path="/" element={authUser?<ChatPage />:<Navigate to="/login"/>}></Route>
+      <Route path="/login" element={!authUser?<LoginPage/>:<Navigate to="/"/>}></Route>
+      <Route path="/signup" element={!authUser?<SignUpPage/>:<Navigate to="/"/>}></Route>
     </Routes>
     </div>
   )
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,33 @@
 import { create } from "zustand";
-
+import {axiosInstance} from "../lib/axios.js"
+import toast from "react-hot-toast";
 export const useAuthStore=create((set,get)=>({
-    authUser:{name:"John",_id:123,age:25},
-    isLoading:false,
-    isLoggedIn:false,
-    login:()=>{
-        console.log("we just logged in")
-        set({isLoggedIn:true,isLoading:true})
+    authUser:null,
+    isCheckingAuth:true,
+    isSigningUp:false,
+    checkAuth:async ()=>{
+        try{
+            const res=await  axiosInstance.get('/auth/check') 
+            set({authUser:res.data}) 
+        
+        }catch(error){
+            console.log("Error",error)
+            set({authUser:null})
+        }finally{
+            set({isCheckingAuth:false})
+        }
+    },
+    signup:async (data)=>{
+        set({isSigningUp:true})
+        try{
+            const res=await  axiosInstance.post('/auth/signup',data)
+            set({authUser:res.data})
+            toast.success("Account created successfully")
+        }catch(error){
+            toast.error(error.response.data.message)
+
+        }finally{
+            set({isSigningUp:false})
+        }
     }
 }))
