d9298ba7bff3fa0f82f6ccdd7347b5f4df0e18fe
sukanyakake <kdmrsukanya@gmail.com>
2025-12-23 18:43:04 +0530

    online-users implemented

--- frontend/src/components/ContactsList.jsx
+++ frontend/src/components/ContactsList.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import NoChatsFound from './NoChatsFound'
 import UsersLoadingSkeleton from './UsersLoadingSkeleton'
+import { useAuthStore } from '../store/useAuthStore'
 
 const ContactsList = () => {
   const {selectedUser,isUsersLoading,getAllContacts,allContacts,setSelectedUser}=useChatStore()
+  const {onlineUsers}=useAuthStore()
   useEffect(()=>{
     getAllContacts()
   },[getAllContacts])
@@ -20,7 +22,7 @@ const ContactsList = () => {
           onClick={() => setSelectedUser(contact)}
         >
           <div className="flex items-center gap-3">
-            <div className={`avatar online`}>
+            <div className={`${onlineUsers.includes(contact._id)?"online":"offline"}`}>
               <div className="size-12 rounded-full">
                 <img src={contact.profilePic || "/avatar.png"} alt={contact.fullName} />
               </div>
